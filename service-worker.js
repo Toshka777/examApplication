eec65aef2d99969e384f68586bca9ba05c8c4e25
Toshka777 <160187743+Toshka777@users.noreply.github.com>
2025-02-27 01:01:02 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
 const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/questions.json',
-  '/exam.png'
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './questions.json',
+  './exam.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(urlsToCache).catch(error => {
+        console.error('Failed to cache:', error);
+      });
     })
   );
 });
